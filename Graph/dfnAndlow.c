2c3ab5c526e6702f5b561f133fdea34cfc9d8654
yiwei <wangyiwei11259@gmail.com>
2018-03-28 18:12:29 +0800

    Remove unused array

--- Graph/dfnAndlow.c
+++ Graph/dfnAndlow.c
@@ -9,9 +9,9 @@ int n;
 int num = 0;
 int startVertex = 3;
 
-void initialize(int visited[], int dfn[], int low[]);
+void initialize(int dfn[], int low[]);
 void read(int graph[][n]);
-void DFS(int child, int parent, int visited[], int graph[][n], int dfn[], int low[]);
+void DFS(int child, int parent, int graph[][n], int dfn[], int low[]);
 void printOut(int dfn[], int low[]);
 
 void main()
@@ -21,30 +21,27 @@ void main()
 	scanf("%d", &n);
 
 	int graph[n][n];
-	int visited[n];
 	int dfn[n];
 	int low[n];
 
-	initialize(visited, dfn, low);
+	initialize(dfn, low);
 
 	printf("\nEnter adjecency matrix of the graph:\n");
 	read(graph);
 	printf("\n");
 
 	printf("Traversal order : ");
-	DFS(startVertex, -1, visited, graph, dfn, low);
+	DFS(startVertex, -1, graph, dfn, low);
 	printf("\n\n");
 	printOut(dfn, low);
 }
 
-void initialize(int visited[], int dfn[], int low[])
+void initialize(int dfn[], int low[])
 {
 	int i;
 	for (i = 0; i < n; i++)
-	{
-		visited[i] = 0;
 		dfn[i] = low[i] = -1;
-	}
+	
 }
 
 void read(int graph[][n])
@@ -55,33 +52,29 @@ void read(int graph[][n])
 			scanf("%d", &graph[i][j]);
 }
 
-void DFS(int child, int parent, int visited[], int graph[][n], int dfn[], int low[])
+void DFS(int child, int parent, int graph[][n], int dfn[], int low[])
 {
 	int adj_vertex;
 	int new_child;
 	if(child == startVertex) printf("%d", child);
 	else printf(" => %d", child);
 
-	// assign dfn and low
+	// assign value to dfn and low
 	dfn[child] = low[child] = num++;
 
 	for (new_child = 0; new_child < n; new_child++)
+		// if not visited and there exists a path to the vertex
 		if (dfn[new_child] < 0 && graph[child][new_child] == exist)
 		{
-			DFS(new_child, child, visited, graph, dfn, low);
+			DFS(new_child, child, graph, dfn, low);
 
-			// calculate low[] when going back
+			// calculate value of low when going back
 			for (adj_vertex = 0; adj_vertex < n; ++adj_vertex)
-			{
+				// if there exists a back edge to my ancestor vertex
 				if (graph[child][adj_vertex] == back_edge && dfn[child] > dfn[adj_vertex])
-				{
-					// printf("child = %d\t", child);
-					// printf("dfn[%d] = %d\n", adj_vertex, dfn[adj_vertex]);
 					low[child] = dfn[adj_vertex];
-				}
-			}
 
-			// if my child has lower low , update my low
+			// if my child has lower value of low , update my value of low
 			low[child] = MIN2(low[child], low[new_child]);
 		}
 }
